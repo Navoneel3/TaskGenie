970c92722ecb188b228705885afce2b289617f9b
Navoneel Dey <navoneeldey3@gmail.com>
2026-06-02 23:19:41 +0530

    Chore:Created TaskController and implemented createTask.

---
+++ src/main/java/com/example/TaskApp/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.example.TaskApp.controller;
+
+import com.example.TaskApp.domain.CreateTaskRequest;
+import com.example.TaskApp.domain.dto.CreateTaskRequestDto;
+import com.example.TaskApp.domain.dto.TaskDto;
+import com.example.TaskApp.domain.entity.Task;
+import com.example.TaskApp.mapper.TaskMapper;
+import com.example.TaskApp.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+public class TaskController {
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    public TaskController(TaskService taskService,TaskMapper taskMapper){
+        this.taskService=taskService;
+        this.taskMapper=taskMapper;
+
+    }
+    @PostMapping
+    public ResponseEntity<TaskDto> createTask(@Valid @RequestBody CreateTaskRequestDto createTaskRequestDto){
+        CreateTaskRequest createTaskRequest = taskMapper.fromDto(createTaskRequestDto);
+        Task task=taskService.createTask(createTaskRequest);
+        TaskDto taskDto=taskMapper.toDto(task);
+        return new ResponseEntity<>(taskDto, HttpStatus.CREATED);
+    }
+}

d6cf5537007d7693a88e79a45e3dc218cb1a3c44
Navoneel Dey <navoneeldey3@gmail.com>
2026-06-02 20:16:33 +0530

    Chore:Created Mapper and Implemented it.

---
+++ src/main/java/com/example/TaskApp/mapper/TaskMapper.java
@@ -0,0 +1,12 @@
+package com.example.TaskApp.mapper;
+
+import com.example.TaskApp.domain.CreateTaskRequest;
+import com.example.TaskApp.domain.dto.CreateTaskRequestDto;
+import com.example.TaskApp.domain.dto.TaskDto;
+import com.example.TaskApp.domain.entity.Task;
+
+public interface TaskMapper {
+    CreateTaskRequest fromDto(CreateTaskRequestDto dto);
+
+    TaskDto toDto(Task task);
+}
---
+++ src/main/java/com/example/TaskApp/mapper/impl/TaskMapperImpl.java
@@ -0,0 +1,35 @@
+package com.example.TaskApp.mapper.impl;
+
+import com.example.TaskApp.domain.CreateTaskRequest;
+import com.example.TaskApp.domain.dto.CreateTaskRequestDto;
+import com.example.TaskApp.domain.dto.TaskDto;
+import com.example.TaskApp.domain.entity.Task;
+import com.example.TaskApp.mapper.TaskMapper;
+
+public class TaskMapperImpl implements TaskMapper {
+
+    @Override
+    public CreateTaskRequest fromDto(CreateTaskRequestDto dto) {
+        return new CreateTaskRequest(
+                dto.title(),
+                dto.description(),
+                dto.dueDate(),
+                dto.priority()
+        );
+    }
+
+    @Override
+    public TaskDto toDto(Task task) {
+        if(null==task){
+            return null;
+        };
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                task.getPriority(),
+                task.getStatus()
+        );
+    }
+}
